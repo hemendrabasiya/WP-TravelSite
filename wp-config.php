c97c6ad8fabdb196fb5b0974d5cf3529ac506726
hemendrabasiya <107398685+hemendrabasiya@users.noreply.github.com>
2023-11-18 22:01:43 +0530

    front-page style

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,22 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'jetjourney' );
+define('DB_NAME', 'dreamfly');
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define('DB_USER', 'root');
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define('DB_PASSWORD', '');
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define('DB_HOST', 'localhost');
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define('DB_CHARSET', 'utf8mb4');
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication unique keys and salts.
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'w)CAZsSQD*IRceM4J^~!J40GQysI-JMv?{EwF=#LQAFg& .BS5.sf%dWI:l~~F]=' );
-define( 'SECURE_AUTH_KEY',  'O:rpK(NJ?ljC!QnapwoK -}C0u6YBMm[~p2EmTx5zbIAz.Ui7w0qh=dpV&h91OW|' );
-define( 'LOGGED_IN_KEY',    ')rJ,=6[f1uCB}tIQ#ZC;L~50g&1Bl0$3+Wnke)|x4lsWy9$Q v8_/RE)[+5Q^J@F' );
-define( 'NONCE_KEY',        '&|RN#QB,`5PY1![q^?;{m7GNz~c[]wsBXg:4tjAs9H9zUAk&u@b)|l9Z1GZJ7(lr' );
-define( 'AUTH_SALT',        '*E|EN}Y.Qk>:uO#|8~,5=6I[Ds+6`MI_;tqo+]Kx2i.80TNBJB_MyQb^%J6>W.04' );
-define( 'SECURE_AUTH_SALT', '5H$z1L]k68WvR>6d$J FlkS<d>j^8zjMvCPWSiWIGCa_m!q)Cf}-5`gqrkBE1;~7' );
-define( 'LOGGED_IN_SALT',   'w3VS~*MhFQCa=?xpHNTsgv0];X;=yootABs>F?B7p,829oZUc@W7oE{QYL|T^:$M' );
-define( 'NONCE_SALT',       '^a3QeZ<VbV^8o/5Z?rqeU*xWiI_DdRGxNHtWy$bg]Ur:NhWV**m~08NZ&We:OQ4:' );
+define('AUTH_KEY', 'w)CAZsSQD*IRceM4J^~!J40GQysI-JMv?{EwF=#LQAFg& .BS5.sf%dWI:l~~F]=');
+define('SECURE_AUTH_KEY', 'O:rpK(NJ?ljC!QnapwoK -}C0u6YBMm[~p2EmTx5zbIAz.Ui7w0qh=dpV&h91OW|');
+define('LOGGED_IN_KEY', ')rJ,=6[f1uCB}tIQ#ZC;L~50g&1Bl0$3+Wnke)|x4lsWy9$Q v8_/RE)[+5Q^J@F');
+define('NONCE_KEY', '&|RN#QB,`5PY1![q^?;{m7GNz~c[]wsBXg:4tjAs9H9zUAk&u@b)|l9Z1GZJ7(lr');
+define('AUTH_SALT', '*E|EN}Y.Qk>:uO#|8~,5=6I[Ds+6`MI_;tqo+]Kx2i.80TNBJB_MyQb^%J6>W.04');
+define('SECURE_AUTH_SALT', '5H$z1L]k68WvR>6d$J FlkS<d>j^8zjMvCPWSiWIGCa_m!q)Cf}-5`gqrkBE1;~7');
+define('LOGGED_IN_SALT', 'w3VS~*MhFQCa=?xpHNTsgv0];X;=yootABs>F?B7p,829oZUc@W7oE{QYL|T^:$M');
+define('NONCE_SALT', '^a3QeZ<VbV^8o/5Z?rqeU*xWiI_DdRGxNHtWy$bg]Ur:NhWV**m~08NZ&We:OQ4:');
 
 /**#@-*/
 
@@ -79,7 +79,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', false);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
@@ -88,8 +88,8 @@ define( 'WP_DEBUG', false );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (!defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
 }
 
 /** Sets up WordPress vars and included files. */
--- wp-content/themes/dreamfly-theme/front-page.php
+++ wp-content/themes/dreamfly-theme/front-page.php
@@ -3,6 +3,12 @@
 <main class="landing-page container">
 
     <section class="main-section">
+        <div class="content-section">
+            <h1>Embark on Your Ultimate Journey with Us</h1>
+            <p>Craft Your Dream Vacation with Expert Advice, Travel Tips, Destination Info, and Inspiring Ideas from Our
+                Travel Enthusiasts.</p>
+            <button>Reveal Now</button>
+        </div>
         <div class="main-bg">
             <video playsinline autoplay muted loop>
                 <source src="./wp-content/themes/dreamfly-theme/images/vid_5.mp4" type="video/mp4">
